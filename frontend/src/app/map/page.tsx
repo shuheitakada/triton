41189c9a5c712b98580da24a587c4fd2daafa023
shuheitakada <takada.shuhei626@gmail.com>
2023-11-07 20:39:28 +0900

    feat: とりあえずGoogle Mapを表示させるところまで

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,3 +1,24 @@
+'use client'
+
+import GoogleMapReact from 'google-map-react'
+
 export default function Map() {
-  return <h1>Map Page</h1>
+  const defaultLatLng = {
+    lat: 35.6812358,
+    lng: 139.7568251,
+  }
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string
+
+  return (
+    <>
+      <h1>Map Page</h1>
+      <div style={{ height: '500px', width: '500px' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={defaultLatLng}
+          defaultZoom={16}
+        />
+      </div>
+    </>
+  )
 }
