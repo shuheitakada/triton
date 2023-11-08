9db3536ce9ff870ee63742f1d23cfe410ab508dc
shuheitakada <takada.shuhei626@gmail.com>
2023-11-08 21:27:49 +0900

    feat: google mapで検索してヒットしたところにピンを指す

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -4,8 +4,10 @@ import GoogleMapReact from 'google-map-react'
 import { useState } from 'react'
 
 export default function Map() {
-  const [map, setMap] = useState<google.maps.Map>()
+  const [map, setMap] = useState<any>()
   const [maps, setMaps] = useState<any>()
+  const [geocoder, setGeocoder] = useState<google.maps.Geocoder>()
+  const [address, setAddress] = useState<any>()
   const [marker, setMarker] = useState<any>()
 
   const defaultLatLng = {
@@ -14,21 +16,33 @@ export default function Map() {
   }
   const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string
 
-  const handleApiLoaded = ({
-    map,
-    maps,
-  }: {
-    map: google.maps.Map
-    maps: google.maps.MarkerLibrary
-  }) => {
+  const handleApiLoaded = ({ map, maps }: { map: google.maps.Map; maps: any }) => {
     setMap(map)
     setMaps(maps)
-    setMarker(
-      new maps.Marker({
-        map,
-        position: defaultLatLng,
-      }),
-    )
+    setGeocoder(new maps.Geocoder())
+  }
+
+  const search = () => {
+    if (!geocoder) {
+      return
+    }
+
+    geocoder.geocode({ address }, (results: any, status: any) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location)
+        if (marker) {
+          marker.setMap(null)
+        }
+        setMarker(
+          new maps.Marker({
+            map,
+            position: results[0].geometry.location,
+          }),
+        )
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status)
+      }
+    })
   }
 
   const setLatLng = ({ lat, lng }: { lat: number; lng: number; event: any }) => {
@@ -50,6 +64,22 @@ export default function Map() {
   return (
     <>
       <h1>Map Page</h1>
+      {/* 検索フォーム */}
+      <div>
+        <input
+          type='text'
+          onChange={(e) => setAddress(e.target.value)}
+          className='shadow border text-gray-700 rounded p-2 m-1 mr-0'
+        />
+        <button
+          type='button'
+          onClick={search}
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 m-1 ml-0'
+        >
+          Search
+        </button>
+      </div>
+      {/* マップ */}
       <div style={{ height: '500px', width: '500px' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: API_KEY }}
